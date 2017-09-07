d288c8e43b5100c450a58b7251c7069473b18ebf
linyuxiaoran <420343280@qq.com>
2017-09-07 11:39:02 +0800

    2017-9-7 11:38:59

---
+++ Project1/090201.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main1() {
+	cout << "Hello world!1222" << endl;
+	return 0;
+}
---
+++ Project1/090301.cpp
@@ -0,0 +1,13 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main090301() {
+//	cout << "Size of char:" << sizeof(char) << endl;
+//	cout << "Size of int:" << sizeof(int) << endl;
+//	cout << "Size of short int:" << sizeof(short int) << endl;
+//	cout << "Size of Long int: " << sizeof(long int) << endl;
+//	cout << "Size of float:" << sizeof(float) << endl;
+//	cout << "Size of double:" << sizeof(double) << endl;
+//	cout << "Size of wchar_t:" << sizeof(wchar_t) << endl;
+//	return 0;
+//}
