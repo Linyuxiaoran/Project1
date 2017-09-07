d8c590fa03e186252f8e239cb7941ef7e9954079
linyuxiaoran <420343280@qq.com>
2017-09-07 17:16:00 +0800

    C++运算符

---
+++ Project1/090706.cpp
@@ -0,0 +1,27 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	int a = 21;
+//	int b = 10;
+//	int c;
+//
+//	c = a + b;
+//	cout << "Line 1 -c:" << c << endl;
+//	c = a - b;
+//	cout << "Line 2 -c:" << c << endl;
+//	c = a * b;
+//	cout << "Line 3 -c:" << c << endl;
+//	c = a / b;
+//	cout << "Line 4 -c:" << c << endl;
+//	c = a % b;
+//	cout << "Line 5 -c:" << c << endl;
+//
+//	int d = 10;
+//	c = d++;
+//	cout << "Line 6 -c:" << c << endl;
+//	d = 10;
+//	c = d--;
+//	cout << "Line 7 -c:" << c << endl;
+//	return 0;
+//}
