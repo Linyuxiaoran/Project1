0c093152301d32ad7696d344189a0d8a2cc663a3
linyuxiaoran <420343280@qq.com>
2017-09-19 17:04:19 +0800

    C++ 数字

---
+++ Project1/091911.cpp
@@ -0,0 +1,23 @@
+////C++ 随机数
+//#include <iostream>
+//#include <ctime>
+//#include <cstdlib>
+//
+//using namespace std;
+//
+//int main() {
+//	int i, j;
+//
+//	//设置种子
+//	srand((unsigned)time(NULL));
+//
+//	//生成10个随机数
+//	for (i = 0; i < 10; i++) {
+//		//生成实际的随机数
+//		//%100产生100以内的随机数
+//		j = rand()%100;
+//		cout << "随机数: " << j << endl;
+//	}
+//
+//	return 0;
+//}
